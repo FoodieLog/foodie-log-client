e33582b74402d592431539e3675f3ca37a02c118
ChoEun-Sang <wh94es@gmail.com>
2023-08-20 21:34:58 +0900

    feat: signup 페이지 - Input / btn 컴포넌트 생성

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import Button from "@/src/components/Button";
 function SignUp() {
-  return <div>SignUp</div>;
+  const handleClick = () => {
+    console.log("handleClick");
+  };
+  return (
+    <>
+      <div>
+        <h2>Foodie-Log</h2>
+        <p>맛집 정보를 이용하려면 가입하세요.</p>
+        <form>
+          <input type="text" placeholder="이메일" />
+          <input type="text" placeholder="비밀번호" />
+          <input type="text" placeholder="비밀번호 확인" />
+          <Button label={"회원가입"} onClick={handleClick} />
+          <div>
+            <div></div>
+            <div>또는</div>
+            <div></div>
+          </div>
+          <Button label={"카카오 로그인"} onClick={handleClick} />
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default SignUp;
