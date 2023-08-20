ef8dbedc90a302e090d07c15aa3d66a37df8e40c
ChoEun-Sang <wh94es@gmail.com>
2023-08-20 21:34:05 +0900

    feat: Button 컴포넌트 생성

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+interface IButtonProps {
+  label: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
 
-function Button() {
-  return <div>index</div>;
+function Button({ label, onClick }: IButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={"w-80 mx-2 my-2.5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"}
+    >
+      {label}
+    </button>
+  );
 }
 
 export default Button;
