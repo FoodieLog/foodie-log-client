2bfe7dd1b8a8f1070f42ffc8bbea448317a96d55
iskra17 <rayanami@naver.com>
2023-08-21 08:47:13 +0900

    home 페이지 새성 및, 레이아웃 구성중

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,8 @@
 export default function Home() {
   return (
     <section>
+      {/* Searh  / Notification */} 
+      {/* Posts */}
       <h1 className="text-green-500">Hello World!!!!!</h1>
     </section>
   );
