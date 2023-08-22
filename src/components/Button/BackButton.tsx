a2f5cb6547f3d0fc0ccaabf473a35600bd14e9b9
ChoEun-Sang <wh94es@gmail.com>
2023-08-22 12:25:55 +0900

    feat: 뒤로가기 버튼 컴포넌트 구현

---
+++ src/components/Button/BackButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BsArrowLeftCircle } from "react-icons/bs";
+
+function BackButton() {
+  return (
+    <div className="absolute top-8">
+      <button type="button" className="w-20">
+        <BsArrowLeftCircle size="1.7rem" className="hover:text-orange-600" />
+      </button>
+    </div>
+  );
+}
+
+export default BackButton;
