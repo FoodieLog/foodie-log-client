1d2dc8eeb23a851a22ddad0f601fa3db274e581b
iskra17 <rayanami@naver.com>
2023-08-23 17:05:18 +0900

    Feat : home 페이지 스크롤에 따른 상단바 표시 변경, Feed 좌우 패딩적용

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,11 +1,33 @@
+"use client";
 import Image from "next/image";
 import { Logo } from "@/public/images";
 import { IoIosSearch, IoMdNotificationsOutline } from "react-icons/io";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Topbar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      setIsVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
+
   return (
-    <section className="flex justify-between items-center max-w-[640px] px-4 bg-cyan-500">
+    // <section className="flex justify-between items-center max-w-[640px] px-4 bg-cyan-500">
+    <section
+      className={`z-20 flex justify-between items-center max-w-[640px] px-4 bg-cyan-500 
+    ${isVisible ? "top-0" : "-top-16"} sticky transition-top duration-300`}
+    >
       <div>
         <Image src={Logo} alt="logo" className="w-[96px] h-[36px] my-2 sm:hidden cursor-pointer" />
       </div>
