d32fd0dfc6412aba419d060d28e0804c7be778e8
iskra17 <rayanami@naver.com>
2023-08-23 15:56:14 +0900

    Fix : RootLayout 수정 및 main, accounts 폴더 하단에 새로운 Layout 생성. 기존 폴더 위치 변경

---
+++ src/app/accounts/layout.tsx
@@ -0,0 +1,7 @@
+export default function AccountsLayout({ children }: { children: React.ReactNode }) {
+  return (
+  <div className="flex w-full">
+    <div className="w-screen h-screen flex justify-center mt-10">{children}</div>;
+  </div>
+  )
+}
