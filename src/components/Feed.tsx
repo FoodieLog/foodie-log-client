77a2970baca279f01ad88a2071402a5ac513a35e
iskra17 <rayanami@naver.com>
2023-08-24 14:26:01 +0900

    Refactor : ShopCard 컴포넌트 생성. 재활용을 위해 따로 빼서 정리함

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -8,8 +8,8 @@ import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { FaRegCommentDots } from "react-icons/fa";
 import { FiShare2 } from "react-icons/fi";
 import { getIcon } from "../utils/iconUtils";
-import Button from './Button';
-
+import Button from "./Button";
+import ShopCard from './ShopCard';
 
 const Feed: React.FC<FeedData> = ({ feed, restaurant, isFollowed, isLiked }) => {
   const timeDifference = getTimeDiff(dayjs(feed.createdAt));
@@ -19,13 +19,11 @@ const Feed: React.FC<FeedData> = ({ feed, restaurant, isFollowed, isLiked }) => 
 
   // 주의 : 이미지 경로를 /public/images/... 로 시작하면 안된다.
   // 대부분의 프론트엔드 프레임워크나 빌드 도구에서는 public 디렉토리의 내용이 빌드 시 루트 경로(/)에 배포된다고 한다.
-  const shopCategoryIcon = `/images/foodCategoryIcons/${getIcon(restaurant.category)}`
-  
+  const shopCategoryIcon = `/images/foodCategoryIcons/${getIcon(restaurant.category)}`;
+
   const handleButtonClick = () => {
     setFollow(!Follow);
   };
-
-
 
   return (
     <div className="mt-2 w-full max-w-[640px] bg-mint-light border rounded-sm px-2">
@@ -41,8 +39,11 @@ const Feed: React.FC<FeedData> = ({ feed, restaurant, isFollowed, isLiked }) => 
           <p className="text-sm">{timeDifference}</p>
         </div>
         {/* isFollowed 가 true 면 버튼 label이 "팔로잉", 아니면 "팔로우" */}
-        <button className="w-30 h-9 py-2 mr-4 px-4 text-white font-bold rounded-2xl 
-        bg-green-400 hover:bg-green-500 border-0" onClick={handleButtonClick}>
+        <button
+          className="w-30 h-9 py-2 mr-4 px-4 text-white font-bold rounded-2xl 
+        bg-green-400 hover:bg-green-500 border-0"
+          onClick={handleButtonClick}
+        >
           {Follow ? "팔로잉" : "팔로우"}
         </button>
         {/* <Button variant={"primary"} size={"w-30 h-9"} onClick={handleButtonClick}>
@@ -53,25 +54,21 @@ const Feed: React.FC<FeedData> = ({ feed, restaurant, isFollowed, isLiked }) => 
       {/* image */}
       <ImageSlide images={feed.feedImages} />
       {/* restaurant */}
-      <div className='flex items-center'>
-      <img
-          src={shopCategoryIcon}
-          alt="음식점 썸네일"
-          className="w-12 h-12 border p-1 rounded-full cursor-pointer"
-        />
-      <div className="flex flex-col items-start p-3">
-        <p className="font-bold cursor-pointer">{restaurant.name}</p>
-        <p className="text-sm cursor-pointer">{restaurant.roadAddress}</p>
-      </div>
-      </div>
+      <ShopCard
+        name={restaurant.name}
+        category={restaurant.category}
+        roadAddress={restaurant.roadAddress}
+      />
       {/* content */}
       <p className="p-1">{feed.content}</p>
       <div className="flex flex-between gap-2 items-center text-[18px] p-3">
-        <button className="text-[24px]" onClick={() => setLike(!Like)}>{Like ? <AiFillHeart /> : <AiOutlineHeart />}</button>
+        <button className="text-[24px]" onClick={() => setLike(!Like)}>
+          {Like ? <AiFillHeart /> : <AiOutlineHeart />}
+        </button>
         <p>{feed.likeCount}</p>
-        <FaRegCommentDots className="text-[24px] cursor-pointer"/>
+        <FaRegCommentDots className="text-[24px] cursor-pointer" />
         <p className="flex-1">{feed.replyCount}</p>
-        <FiShare2  className="text-[24px] cursor-pointer"/>
+        <FiShare2 className="text-[24px] cursor-pointer" />
       </div>
     </div>
   );
--- src/components/ShopThumb.tsx
+++ src/components/ShopThumb.tsx
@@ -1,18 +1,24 @@
 import { ShopThumbData } from "../types/apiTypes";
 import { getIcon } from "../utils/iconUtils";
+import ShopCard from './ShopCard';
 
 const ShopThumb: React.FC<ShopThumbData> = ({ id, name, category, roadAddress, feedList }) => {
   const shopCategoryIcon = `/images/foodCategoryIcons/${getIcon(category)}`;
 
   return (
     <div className="mt-2 w-full max-w-[640px] bg-gray-100 border rounded-sm px-1">
-      <div className="flex items-center">
+      
+      <ShopCard name={name} category={category} roadAddress={roadAddress} />
+
+      {/* <div className="flex items-center">
         <img src={shopCategoryIcon} alt="음식점 썸네일" className="w-12 h-12 border p-1 rounded-full cursor-pointer" />
         <div className="flex flex-col items-start p-1">
           <p className="font-bold text-base cursor-pointer">{name}</p>
-          <p className="text-base cursor-pointer">{roadAddress}</p>
+          <p className="text-sm cursor-pointer">{roadAddress}</p>
         </div>
-      </div>
+      </div> */}
+      
+      
       {/* <p className="text-sm p-1">{category}</p> */}
 
       <div className="flex gap-1">
