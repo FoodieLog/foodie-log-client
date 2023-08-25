79ae231158568653ed81bcaff97862ad8137b1e7
iskra17 <rayanami@naver.com>
2023-08-25 21:23:16 +0900

    Move : 카카오 지도 script를 RootLayout 에서 MainLayout 으로 이동시킴

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -10,9 +9,12 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+  
   return (
     <html lang="ko">
       <body className={inter.className}>
+
         <div>{children}</div>
       </body>
     </html>
--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,10 +1,20 @@
 import LeftSidebar from "../../components/LeftSidebar";
 import Bottombar from "../../components/Bottombar";
+import Script from "next/script";
+const NEXT_PUBLIC_KAKAO_MAP_API_KEY = process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY;
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
+  if (!NEXT_PUBLIC_KAKAO_MAP_API_KEY) {
+    throw new Error("KAKAO MAP API KEY is not defined!");
+  }
+
   return (
     <>
-      <div className="flex w-full">
+      <Script
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services,clusterer&autoload=false`}
+          strategy="beforeInteractive"
+        />
+      <div className="flex mx-auto">
         <LeftSidebar />
         <div className="w-full h-full flex justify-center">{children}</div>
       </div>
