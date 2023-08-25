26fed16bbdf35ef96ee15d27619e9fc50e5effe2
iskra17 <rayanami@naver.com>
2023-08-25 21:29:16 +0900

    Feat : 식당 상세정보 페이지(RestaurantDetail) 레이아웃 구현 완료

---
+++ src/app/main/restaurants/[id]/page.tsx
@@ -0,0 +1,11 @@
+import RestaurantDetail from '@/src/components/RestaurantDetail'
+
+const restaurant = () => {
+  return (
+    <div className='w-full bg-orange-500 flex justify-center'>
+      <RestaurantDetail />
+    </div>
+  )
+}
+
+export default restaurant
