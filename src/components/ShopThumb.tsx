e437d163580eb7a6721e016bcb60636648bb5afa
iskra17 <rayanami@naver.com>
2023-08-25 21:25:43 +0900

    Feat : 음식점 정보 클릭시 식당 상세정보 페이지로 이동시킴. ShopCard 매개변수에 따른 레이아웃 조정

--- src/components/ShopThumb.tsx
+++ src/components/ShopThumb.tsx
@@ -1,34 +1,30 @@
+import Link from "next/link";
 import { ShopThumbData } from "../types/apiTypes";
 import { getIcon } from "../utils/iconUtils";
-import ShopCard from './ShopCard';
+import ShopCard from "./ShopCard";
+import Image from "next/image";
 
 const ShopThumb: React.FC<ShopThumbData> = ({ id, name, category, roadAddress, feedList }) => {
   const shopCategoryIcon = `/images/foodCategoryIcons/${getIcon(category)}`;
 
   return (
     <div className="mt-2 w-full max-w-[640px] bg-gray-100 border rounded-sm px-1">
-      
-      <ShopCard name={name} category={category} roadAddress={roadAddress} />
-
-      {/* <div className="flex items-center">
-        <img src={shopCategoryIcon} alt="음식점 썸네일" className="w-12 h-12 border p-1 rounded-full cursor-pointer" />
-        <div className="flex flex-col items-start p-1">
-          <p className="font-bold text-base cursor-pointer">{name}</p>
-          <p className="text-sm cursor-pointer">{roadAddress}</p>
-        </div>
-      </div> */}
-      
-      
-      {/* <p className="text-sm p-1">{category}</p> */}
+      <ShopCard id={id} name={name} category={category} roadAddress={roadAddress} />
 
       <div className="flex gap-1">
         {feedList.map((feed) => (
           <div className="flex w-1/3" key={feed.id}>
-            <img
-              src={feed.thumbnailUrl}
-              alt="Feed thumbnail"
-              className="w-full h-full object-cover border rounded cursor-pointer"
-            />
+            <Link href={`/main/restaurants/${id}`}>
+              {/* {console.log(feed.thumbnailUrl)} */}
+              {/* FIXME : img 를 Image 컴포넌트로 바꾸기 */}
+                <img
+                  
+                  src={feed.thumbnailUrl}
+                  alt="Feed thumbnail"
+                  className="w-full h-full object-cover border rounded cursor-pointer"
+                />
+              
+            </Link>
           </div>
         ))}
       </div>
