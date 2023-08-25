1d3d85c49c973d2c9958f470c0dfb237beb334fb
iskra17 <rayanami@naver.com>
2023-08-25 21:07:33 +0900

    Add : 외부 이미지 url 적용을 위한 next.config.js 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloudflare-ipfs.com',
+        port: '',
+        pathname: '/ipfs/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/seed/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
