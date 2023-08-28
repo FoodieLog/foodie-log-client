61626005d2ec09ba3b70f9dc7254e7b7476247bb
ChoEun-Sang <wh94es@gmail.com>
2023-08-28 09:58:13 +0900

    feat: axiosConfig 생성

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,22 @@
-// api config 넣는 파일
+import axios, { AxiosRequestConfig } from "axios";
+
+const BASE_URL = "http://15.165.93.123:8080";
+
+const axiosConfig: AxiosRequestConfig = {
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const multipartConfig: AxiosRequestConfig = {
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+};
+
+const axiosRequest = axios.create(axiosConfig);
+const multipartrequest = axios.create(multipartConfig);
+
+export { axiosRequest, multipartrequest };
