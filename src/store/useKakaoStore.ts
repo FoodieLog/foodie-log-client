5bed90784de5c6b1543966ae1cc2f20cd45855ee
ChoEun-Sang <wh94es@gmail.com>
2023-08-29 22:39:26 +0900

    feat: 카카오 코드 Store 생성

---
+++ src/store/useKakaoStore.ts
@@ -0,0 +1,13 @@
+import create from "zustand";
+
+interface KakaoStore {
+  code: string | null;
+  setCode: (code: string | null) => void;
+}
+
+const useKakaoStore = create<KakaoStore>((set) => ({
+  code: "",
+  setCode: (newData) => set({ code: newData }),
+}));
+
+export default useKakaoStore;
