be3e874f93a9e450bd48b536bc30ad9e4fbee151
iskra17 <rayanami@naver.com>
2023-08-31 12:08:02 +0900

    Add : useUserStore 에 persist middleware를 추가하여 로컬 스토리지와 연결

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,4 +1,5 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 type User = {
   id?: number;
@@ -13,31 +14,40 @@ type UserState = {
   clearUser: () => void;
 };
 
-
-export const useUserStore = create<UserState>((set) => ({
-  user: {
-    id: undefined,
-    nickName: undefined,
-    profileImageUrl: undefined,
-    accessToken: undefined
-  },
-  setUser: (user) => set(prevState => ({
-    ...prevState,
-    user: {
-      id: user.id,
-      nickName: user.nickName,
-      profileImageUrl: user.profileImageUrl,
-      accessToken: user.accessToken
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      user: {
+        id: undefined,
+        nickName: undefined,
+        profileImageUrl: undefined,
+        accessToken: undefined,
+      },
+      setUser: (user: User) =>
+        set(prevState => ({
+          ...prevState,
+          user: {
+            id: user.id,
+            nickName: user.nickName,
+            profileImageUrl: user.profileImageUrl,
+            accessToken: user.accessToken,
+          }
+        })),
+      clearUser: () => 
+        set(prevState => ({
+          ...prevState,
+          user: {
+            id: undefined,
+            nickName: undefined,
+            profileImageUrl: undefined,
+            accessToken: undefined,
+          }
+        }))
+    }),
+    {
+      name: 'user-storage',
+      storage: createJSONStorage(() => localStorage), 
     }
-  })),
+  )
+);
 
-  clearUser: () => set(prevState => ({
-    ...prevState,
-    user: {
-      id: undefined,
-      nickName: undefined,
-      profileImageUrl: undefined,
-      accessToken: undefined
-    }
-  }))
-}));
