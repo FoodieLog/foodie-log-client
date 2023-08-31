84f1e06f59913f51a60c39a9cd4b52b54eb567ed
iskra17 <rayanami@naver.com>
2023-08-31 10:10:25 +0900

    Refactor : UserStore 에서 User state 설정

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,32 +1,43 @@
 import {create} from 'zustand';
-import { UserResponse } from '@/src/types/apiTypes';
 
-type UserState = {
-  id? : number;
+type User = {
+  id?: number;
   nickName?: string;
   profileImageUrl?: string | null;
   accessToken?: string;
-  setUser: (user: UserResponse) => void;
+};
+
+type UserState = {
+  user: User;
+  setUser: (user: User) => void;
   clearUser: () => void;
 };
 
+
 export const useUserStore = create<UserState>((set) => ({
-  id: undefined,
-  nickName: undefined,
-  profileImageUrl: undefined,
-  accessToken: undefined,
-
-  setUser: (user) => set({
-    id: user.id,
-    nickName: user.nickName,
-    profileImageUrl: user.profileImageUrl,
-    accessToken: user.accessToken
-  }),
-
-  clearUser: () => set({
+  user: {
     id: undefined,
     nickName: undefined,
     profileImageUrl: undefined,
     accessToken: undefined
-  })
+  },
+  setUser: (user) => set(prevState => ({
+    ...prevState,
+    user: {
+      id: user.id,
+      nickName: user.nickName,
+      profileImageUrl: user.profileImageUrl,
+      accessToken: user.accessToken
+    }
+  })),
+
+  clearUser: () => set(prevState => ({
+    ...prevState,
+    user: {
+      id: undefined,
+      nickName: undefined,
+      profileImageUrl: undefined,
+      accessToken: undefined
+    }
+  }))
 }));
