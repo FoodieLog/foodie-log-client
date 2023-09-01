00fb9f1dbaf3c3a05590195a7856722b9e98586e
iskra17 <rayanami@naver.com>
2023-09-01 10:28:45 +0900

    Add : mainLayout 에 유저인증(authCheck) 적용

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,6 +1,7 @@
-import LeftSidebar from "../../components/LeftSidebar";
-import Bottombar from "../../components/Bottombar";
+import LeftSidebar from "../../components/Main/LeftSidebar";
+import Bottombar from "../../components/Main/Bottombar";
 import Script from "next/script";
+import AuthCheck from "@/src/components/Auth/AuthCheck";
 
 const NEXT_PUBLIC_KAKAO_MAP_API_KEY = process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY;
 
@@ -11,10 +12,11 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <>
+      <AuthCheck />
       <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services,clusterer&autoload=false`}
-          strategy="beforeInteractive"
-        />
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services,clusterer&autoload=false`}
+        strategy="beforeInteractive"
+      />
       <div className="flex mx-auto">
         <LeftSidebar />
         <div className="w-full h-full flex justify-center">{children}</div>
