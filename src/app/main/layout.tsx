6df4ad199267906371ea4435e5bd43e07696515b
ChoEun-Sang <wh94es@gmail.com>
2023-09-02 17:05:45 +0900

    fix: 페이지 전환 에러에 의한 주석처리

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -12,7 +12,7 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <>
-      <AuthCheck />
+      {/* <AuthCheck /> */}
       <Script
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services,clusterer&autoload=false`}
         strategy="beforeInteractive"
