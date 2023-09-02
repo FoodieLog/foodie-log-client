4848b6b98ac27a5233bcdcd9b670c61b8a3eda5f
iskra17 <rayanami@naver.com>
2023-09-02 22:07:41 +0900

    Feat : Search 페이지 구현 완료

--- src/app/main/search/page.tsx
+++ src/app/main/search/page.tsx
@@ -1,8 +1,14 @@
+import BackButtonMain from "@/src/components/Button/BackButtonMain";
+import SearchUser from "@/src/components/SearhUser/SearhUser";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
-
-export default Search
+    <div className="w-full flex flex-col justify-center max-w-screen-sm mx-auto">
+      <div className='flex flex-col items-start justify-center'>
+        <BackButtonMain />
+        <SearchUser />
+      </div>
+    </div>
+  );
+};
+export default Search;
