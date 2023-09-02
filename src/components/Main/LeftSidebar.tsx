ffbde4f5b653962a7fe43500122ce47cddecc137
iskra17 <rayanami@naver.com>
2023-09-03 01:56:48 +0900

    Change : AuthCheck 변경

--- src/components/Main/LeftSidebar.tsx
+++ src/components/Main/LeftSidebar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { Logo, LogoIcon } from "@/public/images";
 import Image from "next/image";
-// import Logout from "./Logout";
+import Logout from "./Logout";
 
 const BottomSideBar = () => {
   const router = useRouter();
@@ -29,7 +29,7 @@ const BottomSideBar = () => {
           );
         })}
       </div>
-      <div className="flex px-6 py-2 mb-4">{/* <Logout /> */}</div>
+      <div className="flex px-6 py-2 mb-4"><Logout /></div>
     </section>
   );
 };
