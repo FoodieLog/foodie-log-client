157aa6684966a264ca9eeb25551279c5891608a8
ChoEun-Sang <wh94es@gmail.com>
2023-09-02 16:52:51 +0900

    feat: 게시글 등록에 필요한 데이터 상태관리

---
+++ src/store/usePostStore.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+
+interface PostStore {
+  content: PostData;
+  setContent: (content: PostData) => void;
+  files: File[];
+  setFiles: (files: File[]) => void;
+  previews: string[];
+  setPreviews: (previews: string[]) => void;
+}
+
+interface PostData {
+  id: string;
+  place_name: string;
+  place_url: string;
+  category_name: string;
+  address_name: string;
+  road_address_name: string;
+  phone: string;
+  x: string;
+  y: string;
+}
+
+export const usePostStore = create<PostStore>((set) => ({
+  content: {
+    id: "",
+    place_name: "",
+    place_url: "",
+    category_name: "",
+    address_name: "",
+    road_address_name: "",
+    phone: "",
+    x: "",
+    y: "",
+  },
+  files: [],
+  previews: [],
+  setContent: (selectedContent) => set({ content: selectedContent }),
+  setFiles: (selectedFiles) => set({ files: selectedFiles }),
+  setPreviews: (selectedPreviews) => set({ previews: selectedPreviews }),
+}));
