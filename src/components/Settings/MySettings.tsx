a00fb6b2527a99d42df504a4c9f72b317c3382ab
ChoEun-Sang <wh94es@gmail.com>
2023-09-06 16:36:09 +0900

    feat: SVG 컴포넌트로 변경 및 설정 페이지 스타일 수정

--- src/components/Settings/MySettings.tsx
+++ src/components/Settings/MySettings.tsx
@@ -4,7 +4,9 @@ import { useUserStore } from "../../store/useUserStore";
 import { LiaAngleLeftSolid } from "react-icons/lia";
 import { useRouter } from "next/navigation";
 import { logOut } from "../../services/settings";
+import { BadgeSvg, ChangeSvg, NotificationSvg, OutSvg, WarningSvg } from "../../assets/svgs";
 import SettingModal from "./SettingModal";
+import Toggle from "../Toggle";
 
 function MySettings() {
   const [isClient, setIsClient] = useState(false);
@@ -41,94 +43,41 @@ function MySettings() {
   };
 
   return (
-    <div className=" h-3/5 absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-      <div className="flex flex-col justify-between w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+    <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+      <div className="flex flex-col w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
         <div className="divide-y">
           <div className=" flex flex-col items-center py-5">
             <div className="pb-10 relative w-full flex items-center justify-center">
               <LiaAngleLeftSolid size="1.5rem" className="absolute left-5" />
-              <strong className="text-base">설정 및 개인정보</strong>
+              <p className="text-sm">설정 및 개인정보</p>
             </div>
-            {isClient && <h5 className="text-xl font-medium text-gray-900 dark:text-white">{nickName}</h5>}
+            {isClient && <h5 className="text-mb font-medium text-gray-900 dark:text-white">{nickName}</h5>}
           </div>
-          <div className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-            <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-              <svg
-                className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M8.60124 1.25086C8.60124 1.75459 8.26278 2.17927 7.80087 2.30989C10.1459 2.4647 12 4.41582 12 6.79999V10.25C12 11.0563 12.0329 11.7074 12.7236 12.0528C12.931 12.1565 13.0399 12.3892 12.9866 12.6149C12.9333 12.8406 12.7319 13 12.5 13H8.16144C8.36904 13.1832 8.49997 13.4513 8.49997 13.75C8.49997 14.3023 8.05226 14.75 7.49997 14.75C6.94769 14.75 6.49997 14.3023 6.49997 13.75C6.49997 13.4513 6.63091 13.1832 6.83851 13H2.49999C2.2681 13 2.06664 12.8406 2.01336 12.6149C1.96009 12.3892 2.06897 12.1565 2.27638 12.0528C2.96708 11.7074 2.99999 11.0563 2.99999 10.25V6.79999C2.99999 4.41537 4.85481 2.46396 7.20042 2.3098C6.73867 2.17908 6.40036 1.75448 6.40036 1.25086C6.40036 0.643104 6.89304 0.150421 7.5008 0.150421C8.10855 0.150421 8.60124 0.643104 8.60124 1.25086ZM7.49999 3.29999C5.56699 3.29999 3.99999 4.86699 3.99999 6.79999V10.25L4.00002 10.3009C4.0005 10.7463 4.00121 11.4084 3.69929 12H11.3007C10.9988 11.4084 10.9995 10.7463 11 10.3009L11 10.25V6.79999C11 4.86699 9.43299 3.29999 7.49999 3.29999Z"
-                  fill="currentColor"
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <div>
-              <a href="#" className="font-semibold text-gray-900">
-                알림
-                <span className="absolute inset-0"></span>
-              </a>
-              <p className="mt-1 text-gray-600">회원님과 관련된 소식을 알림 받으세요!</p>
-              <div className="absolute top-5 right-5 ">
-                <label className="relative inline-flex items-center cursor-pointer">
-                  <input type="checkbox" value="" className="sr-only peer" />
-                  <div className=" top-0 w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                </label>
+          <div className="group relative flex gap-x-6 items-center justify-between rounded-lg p-4 hover:bg-gray-50">
+            <div className="flex items-center gap-x-6">
+              <div className="mt-1 flex items-center justify-center">
+                <NotificationSvg width="18" height="18" className="text-gray-600 group-hover:text-indigo-600" />
               </div>
+              <div className="font-semibold text-gray-900">알림</div>
+            </div>
+            <div className="flex items-center mr-5">
+              <Toggle />
             </div>
           </div>
           <div onClick={onClickBadge} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-            <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-              <svg
-                className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59"
-                />
-              </svg>
+            <div className="mt-1 flex items-center justify-center">
+              <BadgeSvg width="18" height="18" className="text-gray-600 group-hover:text-indigo-600" />
             </div>
             <div>
-              <a href="#" className="font-semibold text-gray-900">
-                뱃지 신청
-                <span className="absolute inset-0"></span>
-              </a>
-              <p className="mt-1 text-gray-600">맛잘알 뱃지를 부여합니다!</p>
+              <div className="font-semibold text-gray-900">뱃지 신청</div>
             </div>
           </div>
           <div onClick={onClickPassword} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-            <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-              <svg
-                className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-                />
-              </svg>
+            <div className="mt-1 flex items-center justify-center">
+              <ChangeSvg width="18" height="18" className="text-gray-600 group-hover:text-indigo-600" />
             </div>
             <div>
-              <a href="#" className="font-semibold text-gray-900">
-                비밀번호 변경
-                <span className="absolute inset-0"></span>
-              </a>
-              <p className="mt-1 text-gray-600">3개월마다 변경을 추천합니다!</p>
+              <div className="font-semibold text-gray-900">비밀번호 변경</div>
             </div>
           </div>
         </div>
@@ -140,14 +89,7 @@ function MySettings() {
               onClick={onClickWithdraw}
               className="cursor-pointer flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-400 hover:text-red-400 hover:bg-gray-100"
             >
-              <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M8.4449 0.608765C8.0183 -0.107015 6.9817 -0.107015 6.55509 0.608766L0.161178 11.3368C-0.275824 12.07 0.252503 13 1.10608 13H13.8939C14.7475 13 15.2758 12.07 14.8388 11.3368L8.4449 0.608765ZM7.4141 1.12073C7.45288 1.05566 7.54712 1.05566 7.5859 1.12073L13.9798 11.8488C14.0196 11.9154 13.9715 12 13.8939 12H1.10608C1.02849 12 0.980454 11.9154 1.02018 11.8488L7.4141 1.12073ZM6.8269 4.48611C6.81221 4.10423 7.11783 3.78663 7.5 3.78663C7.88217 3.78663 8.18778 4.10423 8.1731 4.48612L8.01921 8.48701C8.00848 8.766 7.7792 8.98664 7.5 8.98664C7.2208 8.98664 6.99151 8.766 6.98078 8.48701L6.8269 4.48611ZM8.24989 10.476C8.24989 10.8902 7.9141 11.226 7.49989 11.226C7.08567 11.226 6.74989 10.8902 6.74989 10.476C6.74989 10.0618 7.08567 9.72599 7.49989 9.72599C7.9141 9.72599 8.24989 10.0618 8.24989 10.476Z"
-                  fill="currentColor"
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
+              <WarningSvg width="15" height="15" />
               회원탈퇴
             </div>
           </SettingModal>
@@ -156,14 +98,7 @@ function MySettings() {
             onClick={onClickLogOut}
             className="cursor-pointer flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100"
           >
-            <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M3 1C2.44771 1 2 1.44772 2 2V13C2 13.5523 2.44772 14 3 14H10.5C10.7761 14 11 13.7761 11 13.5C11 13.2239 10.7761 13 10.5 13H3V2L10.5 2C10.7761 2 11 1.77614 11 1.5C11 1.22386 10.7761 1 10.5 1H3ZM12.6036 4.89645C12.4083 4.70118 12.0917 4.70118 11.8964 4.89645C11.7012 5.09171 11.7012 5.40829 11.8964 5.60355L13.2929 7H6.5C6.22386 7 6 7.22386 6 7.5C6 7.77614 6.22386 8 6.5 8H13.2929L11.8964 9.39645C11.7012 9.59171 11.7012 9.90829 11.8964 10.1036C12.0917 10.2988 12.4083 10.2988 12.6036 10.1036L14.8536 7.85355C15.0488 7.65829 15.0488 7.34171 14.8536 7.14645L12.6036 4.89645Z"
-                fill="currentColor"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
+            <OutSvg width="15" height="15" />
             로그아웃
           </div>
         </div>
