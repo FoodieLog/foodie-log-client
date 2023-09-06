e12e89398a495ab82a31cedb2ae5a1bfd19d8401
iskra17 <rayanami@naver.com>
2023-09-06 23:49:42 +0900

    Feat : 맛집 추천 페이지 기능 구현 완료

--- src/components/Restaurant/ShopThumb.tsx
+++ src/components/Restaurant/ShopThumb.tsx
@@ -4,23 +4,35 @@ import { getIcon } from "../../utils/iconUtils";
 import ShopCard from "./ShopCard";
 import Image from "next/image";
 
-const ShopThumb: React.FC<ShopThumbData> = ({ id, name, category, roadAddress, feedList }) => {
-  const shopCategoryIcon = `/images/foodCategoryIcons/${getIcon(category)}`;
+type ExtendedShopThumbData = ShopThumbData & {
+  restaurantId: number 
+};
 
+const ShopThumb: React.FC<ExtendedShopThumbData> = ({ restaurantId: id, name, category = "", roadAddress, feedList }) => {
+  console.log(id, name, category, roadAddress, feedList);
   return (
     <div className="mt-2 w-full max-w-[640px] bg-gray-100 border rounded-sm px-1">
       <ShopCard id={id} name={name} category={category} roadAddress={roadAddress} />
-
       <div className="flex gap-1">
         {feedList.map((feed) => (
-          <div className="flex w-1/3" key={feed.id}>
+          <div className="w-1/3" key={feed.id} style={{ position: "relative" }}>
+            <div style={{ paddingBottom: "100%" }}></div> {/* 1:1 비율을 위한 패딩 트릭 */}
             <Link href={`/main/restaurants/${id}`}>
-              {/* {console.log(feed.thumbnailUrl)} */}
-              {/* FIXME : img 를 Image 컴포넌트로 바꾸기 */}
-              <img
+              <Image
                 src={feed.thumbnailUrl}
                 alt="Feed thumbnail"
-                className="w-full h-full object-cover border rounded cursor-pointer"
+                sizes="100vw"
+                fill
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover", // 이미지를 커버 모드로 설정
+                }}
+                onError={(e) => console.error(e.currentTarget.id)}
+                onLoadingComplete={(img) => console.log(img.naturalWidth)}
               />
             </Link>
           </div>
