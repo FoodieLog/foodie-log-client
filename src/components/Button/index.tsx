1fa472f68419e326badba3dd4a1b541f1bd7f0d2
ChoEun-Sang <wh94es@gmail.com>
2023-09-06 16:36:51 +0900

    feat: 기본 버튼 스타일 변경

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -20,7 +20,8 @@ const Button: React.FC<ButtonProps> = ({
 
   switch (variant) {
     case "primary":
-      variantStyles = "w-full bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 rounded-lg";
+      variantStyles =
+        "w-full text-white bg-mint hover:bg-mint-light focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800";
       break;
     case "secondary":
       variantStyles =
