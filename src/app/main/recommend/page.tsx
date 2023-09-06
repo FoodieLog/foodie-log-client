6f24b1d42b413c1cfcd9ef24a22070384a9244ba
iskra17 <rayanami@naver.com>
2023-09-06 23:49:42 +0900

    Feat : 맛집 추천 페이지 기능 구현 완료

--- src/app/main/recommend/page.tsx
+++ src/app/main/recommend/page.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import AreaSelector from "@/src/components/Restaurant/AreaSelector";
-import Bottombar from "@/src/components/Main/Bottombar";
 import ShopThumbList from "@/src/components/Restaurant/ShopThumbList";
+import { RecommendedRestaurant, getRestaurantRecommended } from "@/src/services/apiRestaurant";
 
 const Recommend = () => {
+  const [recommendedRestaurants, setRecommendedRestaurants] = useState<RecommendedRestaurant[]>([]);
+
+  const handleSelectedAreaChange = async (searchQuery: string) => {
+    try {
+      const response = await getRestaurantRecommended(searchQuery);
+      if (response.status === 200) {
+        setRecommendedRestaurants(response.response.restaurantList);
+      } else {
+        console.error("추천 정보 가져오기 실패");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="recommend_container w-full flex flex-col justify-center items-center max-w-6xl mx-auto bg-orange-500">
-      <AreaSelector />
-      <ShopThumbList />
+    <div className="recommend_container w-full flex flex-col justify-center items-center max-w-6xl mx-auto">
+      <AreaSelector onSelectedAreaChange={handleSelectedAreaChange} />
+      <ShopThumbList restaurants={recommendedRestaurants} />
     </div>
   );
 };
+
 
 export default Recommend;
