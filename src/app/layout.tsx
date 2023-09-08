c02ecd0084eecfe082ddd9d4780369b455132c49
iskra17 <rayanami@naver.com>
2023-09-08 10:55:01 +0900

    Config : PWA 관련 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={`${notoSansKr.variable} ${roboto_mono.variable}`}>
         <div>{children}</div>
       </body>
