675e24f85ed1f541980506bd2e96d6de82fb8e27
ChoEun-Sang <wh94es@gmail.com>
2023-09-08 13:13:51 +0900

    refactor: 마이페이지 타입 분리

---
+++ src/types/mypage.ts
@@ -0,0 +1,14 @@
+export interface ThumbnailState {
+  id: number;
+  thumbnailUrl: string;
+}
+
+export interface myProfileState {
+  aboutMe: string;
+  nickName: string;
+  feedCount: number;
+  follower: number;
+  following: number;
+  profileImageUrl: string;
+  userId: number;
+}
