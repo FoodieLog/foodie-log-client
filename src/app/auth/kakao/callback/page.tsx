c44902ae57d0c3c1cfac1c064a7cc01104e8a88a
ChoEun-Sang <wh94es@gmail.com>
2023-09-23 02:08:15 +0900

    feat: 카카오 로그인 - 이메일 중복 체크 페이지 구현

--- src/app/auth/kakao/callback/page.tsx
+++ src/app/auth/kakao/callback/page.tsx
@@ -1,14 +1,50 @@
-import React, { use } from "react";
-import SignUpTerms from "@/src/components/Auth/SignUpTerms";
-import SignUpProfile from "@/src/components/Auth/SignUpProfile";
-import useSignUpStore from "@/src/store/useSignUpStore";
-function KaKaCallback() {
-  const nextComponent = useSignUpStore.getState().nextComponent;
+"use client";
+import { useEffect } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { getKaKaoToken, postKakaoToken, loginKaKaoToken } from "@/src/services/kakao";
+import { useUserStore } from "@/src/store/useUserStore";
+import { initializePushNotifications } from "@/src/components/Notification/PushNotification";
 
-  if (nextComponent === "SignupProfile") {
-    return <SignUpProfile />;
-  }
-  return <SignUpTerms />;
+function KaKaoCode() {
+  const router = useRouter();
+  const params = useSearchParams();
+  const code = params.get("code");
+  const setUser = useUserStore((state) => state.setUser);
+  const setTokenExpiry = useUserStore((state) => state.setTokenExpiry);
+
+  useEffect(() => {
+    const checkUserEmail = async () => {
+      if (!code) return;
+      const { data } = await getKaKaoToken(code);
+      console.log("카카오 토큰", data);
+      await postKakaoToken(data.access_token)
+        .then(async (res) => {
+          console.log("카카오 이메일 중복 체크 성공", res);
+
+          if (res.data.response.isExists) {
+            await loginKaKaoToken(res.data.response.kakaoAccessToken)
+              .then((res) => {
+                setUser(res.data.response);
+                console.log("카카오 로그인 성공", res);
+                const minutesInMilliseconds = 1000 * 60 * 29;
+                const expiryTime = Date.now() + minutesInMilliseconds;
+                setTokenExpiry(expiryTime); // 만료 시간 설정
+                initializePushNotifications();
+
+                router.replace("/main/home");
+              })
+              .catch((error) => console.log("로그인에러", error));
+          } else {
+            router.replace("/auth/kakao");
+            localStorage.setItem("kakaoToken", res.data.response.kakaoAccessToken);
+          }
+        })
+        .catch((err) => console.log("카카오 이메일 중복 체크 실패", err));
+    };
+    checkUserEmail();
+  }, [code]);
+
+  return <div>KaKaoCode</div>;
 }
 
-export default KaKaCallback;
+export default KaKaoCode;
