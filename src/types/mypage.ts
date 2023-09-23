71e93192c3ad1142e82c62b4a1da05386921e962
iskra17 <rayanami@naver.com>
2023-09-23 13:31:08 +0900

    Add : 마이페이지 썸네일 관련 응답데이터 type 변경

--- src/types/mypage.ts
+++ src/types/mypage.ts
@@ -1,6 +1,45 @@
-export interface ThumbnailState {
-  id: number;
+// mypage.ts
+export interface FeedImage {
+  imageUrl: string;
+}
+
+export interface Feed {
+  userId: number;
+  nickName: string;
+  profileImageUrl: string | null;
+  feedId: number;
   thumbnailUrl: string;
+  createdAt: string;
+  updatedAt: string;
+  feedImages: FeedImage[];
+  content: string;
+  likeCount: number;
+  replyCount: number;
+}
+
+export interface Restaurant {
+  id: number;
+  name: string;
+  category: string;
+  link: string;
+  roadAddress: string;
+}
+
+export interface ThumbnailState {
+  feed: Feed;
+  restaurant: Restaurant;
+  followed: boolean;
+  liked: boolean;
+}
+
+export interface myProfileState {
+  aboutMe: string;
+  nickName: string;
+  feedCount: number;
+  follower: number;
+  following: number;
+  profileImageUrl: string;
+  userId: number;
 }
 
 export interface myProfileState {
