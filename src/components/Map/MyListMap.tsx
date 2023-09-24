1f67680573b396ef2124648cb1c23cd3457a4fbf
ChoEun-Sang <wh94es@gmail.com>
2023-09-25 01:32:03 +0900

    feat: 지도 커스텀 오버레이 디자인 수정

--- src/components/Map/MyListMap.tsx
+++ src/components/Map/MyListMap.tsx
@@ -1,8 +1,8 @@
-"use client";
-import { useEffect, useState } from "react";
+import Image from "next/image";
 import { Map, MapMarker, CustomOverlayMap } from "react-kakao-maps-sdk";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { Fragment } from "react";
 interface MapProps {
   mapData: MapItem[];
 }
@@ -24,12 +24,7 @@ interface MapItem {
 }
 
 function MyListMap({ mapData }: MapProps) {
-  const [hydration, setHydration] = useState(false);
   const router = useRouter();
-
-  useEffect(() => {
-    setHydration(true);
-  }, []);
 
   const onMarkerClick = (restaurantId: number) => {
     router.push(`/main/restaurants/${restaurantId}`);
@@ -37,25 +32,45 @@ function MyListMap({ mapData }: MapProps) {
 
   return (
     <div className="w-full h-[360px] p-1 bg-slate-300">
-      {hydration && (
-        <Map center={{ lat: 36.2683, lng: 127.6358 }} style={{ width: "100%", height: "100%" }} level={15}>
-          {mapData.map((data) => {
-            const lat = parseFloat(data.restaurant.mapY);
-            const lng = parseFloat(data.restaurant.mapX);
-            return (
-              <MapMarker
-                key={data.restaurant.id}
+      <Map center={{ lat: 36.2683, lng: 127.6358 }} style={{ width: "100%", height: "100%" }} level={15}>
+        {mapData.map((data) => {
+          const lat = parseFloat(data.restaurant.mapY);
+          const lng = parseFloat(data.restaurant.mapX);
+          return (
+            <Fragment key={data.restaurant.id}>
+              <MapMarker // 마커를 생성합니다
                 position={{ lat, lng }}
                 title={data.restaurant.name}
+                draggable={true}
                 clickable={true}
                 onClick={() => onMarkerClick(data.restaurant.id)}
-              >
-                <span className="center">{data.restaurant.name}</span>
-              </MapMarker>
-            );
-          })}
-        </Map>
-      )}
+                image={{
+                  src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png", // 마커이미지의 주소입니다
+                  size: {
+                    width: 24,
+                    height: 35,
+                  }, // 마커이미지의 크기입니다
+                }}
+              />
+              <CustomOverlayMap yAnchor={1} position={{ lat, lng }}>
+                <div
+                  onClick={() => onMarkerClick(data.restaurant.id)}
+                  style={{
+                    color: "191919",
+                    backgroundColor: "#F6C443",
+                    padding: "0 5px",
+                    borderRadius: "4px",
+                    marginBottom: "36px",
+                    fontSize: "12px",
+                  }}
+                >
+                  <span>{data.restaurant.name}</span>
+                </div>
+              </CustomOverlayMap>
+            </Fragment>
+          );
+        })}
+      </Map>
     </div>
   );
 }
