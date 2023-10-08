571273b2ef65f21d6640419bab44897f7937aa89
ChoEun-Sang <wh94es@gmail.com>
2023-10-09 00:28:11 +0900

    feat: 탈퇴 사유 수정 및 탈퇴 구현

--- src/components/Settings/SettingModal.tsx
+++ src/components/Settings/SettingModal.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import Button from "../Common/Button";
-import { fetchWithdraw } from "@/src/services/settings";
+import { withdraw } from "@/src/services/settings";
 import { SettingModalProps } from "@/src/types/user";
 import { useUserStore } from "@/src/store/useUserStore";
 import { unlinkKaKaoToken } from "@/src/services/kakao";
@@ -26,7 +26,10 @@ function SettingModal({ children }: SettingModalProps) {
   const { toast } = useToast();
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setWithdrawReason(e.target.value);
+    const { id } = e.target;
+    if (id) {
+      setWithdrawReason(e.target.id);
+    }
   };
 
   const onClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -43,25 +46,34 @@ function SettingModal({ children }: SettingModalProps) {
 
     if (!kakaoAccessToken) {
       try {
-        const res = await fetchWithdraw({ withdrawReason });
-        clearUser();
+        const res = await withdraw({ withdrawReason });
+        console.log(res);
+
         router.replace("/accounts/login");
+        clearUser();
         toast({ title: "푸드로그 탈퇴", description: "회원 탈퇴되었습니다." });
       } catch (error) {
+        console.log("탈퇴 실패", error);
         toast({ title: "탈퇴 실패", description: "탈퇴 실패하였습니다." });
       }
     } else {
       try {
-        await fetchWithdraw({ withdrawReason });
         await unlinkKaKaoToken();
-        clearUser();
+
+        const body = { withdrawReason };
+        const response = await withdraw(body);
+
+        console.log("회원탈퇴 ", response);
         router.replace("/accounts/login");
         toast({ title: "푸드로그 탈퇴", description: "회원 탈퇴되었습니다." });
+        clearUser();
       } catch (error) {
         toast({ title: "탈퇴 실패", description: "탈퇴 실패하였습니다." });
       }
     }
   };
+
+  console.log("reasons", withdrawReason);
 
   return (
     <Dialog>
@@ -73,18 +85,72 @@ function SettingModal({ children }: SettingModalProps) {
             탈퇴한 이메일로 재가입이 불가능합니다. <br /> 그래도 탈퇴하시겠습니까?
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <label htmlFor="name" className="text-right">
+        <div className="">
+          <div className="">
+            <label htmlFor="name" className="text-right flex-shrink-0 text-sm w-10">
               이메일
             </label>
-            <Input id="name" value={email} className="col-span-3" disabled />
+            <Input id="name" value={email} className="col-span-3 border border-none text-sm" disabled />
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <label htmlFor="username" className="text-right">
-              탈퇴사유<span className="text-red-500">*</span>
+          <div className="">
+            <p className="text-sm">
+              탈퇴 사유<span className="text-red-500">*</span>
+            </p>
+            <label htmlFor="UNSATISFACTORY_SUPPORT" className="flex gap-2 items-center text-sm">
+              <Input
+                type="radio"
+                id="UNSATISFACTORY_SUPPORT"
+                name="reason"
+                value={withdrawReason}
+                onChange={onChange}
+                className="w-[10px]"
+              />
+              고객 지원이 만족스럽지 않아서
             </label>
-            <Input id="username" name="reason" value={withdrawReason} onChange={onChange} className="col-span-3" />
+            <label htmlFor="INFREQUENTLY_USED" className="flex gap-2 items-center text-sm">
+              <Input
+                type="radio"
+                id="INFREQUENTLY_USED"
+                name="reason"
+                value={withdrawReason}
+                onChange={onChange}
+                className="w-[10px]"
+              />
+              자주 이용하지 않아서
+            </label>
+            <label htmlFor="USE_OTHER_SITES" className="flex gap-2 items-center text-sm">
+              <Input
+                type="radio"
+                id="USE_OTHER_SITES"
+                name="reason"
+                value={withdrawReason}
+                onChange={onChange}
+                className="w-[10px]"
+              />
+              비슷한 타 사이트를 이용하기 위해서
+            </label>
+            <label htmlFor="ADVERTISEMENT" className="flex gap-2 items-center text-sm">
+              <Input
+                type="radio"
+                id="ADVERTISEMENT"
+                name="reason"
+                value={withdrawReason}
+                onChange={onChange}
+                className="w-[10px]"
+              />
+              광고가 많아서
+            </label>
+            <label htmlFor="ETC" className="flex gap-2 items-center text-sm">
+              <Input
+                type="radio"
+                id="ETC"
+                name="reason"
+                value={withdrawReason}
+                onChange={onChange}
+                className="w-[10px]"
+              />
+              기타
+            </label>
           </div>
         </div>
         <DialogFooter>
