1f23b7c590433f9f11e82e75e50c41314309d96e
iskra17 <rayanami@naver.com>
2023-10-16 11:13:31 +0900

    Feat : 알림페이지에서 피드 좋아요 알림 클릭시 불러오는 싱글 페이지 레이아웃 구현

---
+++ src/app/main/feed/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import BackButtonMain from "@/src/components/Common/Button/BackButtonMain";
+import Feeds from "@/src/components/Feed/Feeds";
+import { useSearchParams } from "next/navigation";
+
+interface userFeedListProps {
+  params: {
+    id: string;
+  };
+}
+
+const UserSingleFeed = () => {
+  // 여기서 params가 아니라 쿼리스트링에서 값을 추출해야함.
+  // 여기에서 필요없는 부분은 지워주고 쿼리스트링에서 singleFeedId를 추출해오는 로직도 구현해줘.
+  const params = useSearchParams();
+  const feedId = params.get("feedId") || undefined;
+
+  console.log("router.query:", feedId);
+  const singleFeedId = feedId ? Number(feedId) : undefined;
+
+  return (
+    <div className="w-full max-w-[640px] mx-auto">
+      <BackButtonMain />
+      <Feeds singleFeedId={singleFeedId} />
+    </div>
+  );
+};
+
+export default UserSingleFeed;
