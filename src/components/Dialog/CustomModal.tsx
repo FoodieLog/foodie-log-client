21daf999c98bc355f564af28c7528732913791d3
ChoEun-Sang <wh94es@gmail.com>
2023-10-19 22:37:11 +0900

    refactor: 모달 백그라운 스타일 수정

--- src/components/Dialog/CustomModal.tsx
+++ src/components/Dialog/CustomModal.tsx
@@ -28,7 +28,7 @@ function CustomModal({ children, showModal, setShowModal, reload }: ModalProps) 
   return (
     <div
       onClick={onClickBg}
-      className="fixed w-screen flex justify-center items-center top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      className="bg-gray-300 bg-opacity-50 fixed w-screen h-screen flex justify-center items-center top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 max-h-full"
     >
       <div className="relative w-full max-w-2xl max-h-full">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">{children}</div>
