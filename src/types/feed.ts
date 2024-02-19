dc7440c596f774dafcb3ca1241ad53ebd4a5e422
ChoEun-Sang <wh94es@gmail.com>
2024-02-19 23:44:42 +0900

    refactor: feed, reply 컴포넌트 TYPE 분리

--- src/types/feed.ts
+++ src/types/feed.ts
@@ -27,3 +27,65 @@ export interface FeedImageSlideProps {
 export interface ReplyListProps {
   id: string;
 }
+
+export type Content = {
+  feed: {
+    userId: number;
+    nickName: string;
+    profileImageUrl: string | null;
+    feedId: number;
+    createdAt: string;
+    updatedAt: string;
+    feedImages: { imageUrl: string }[];
+    content: string;
+    likeCount: number;
+    replyCount: number;
+  };
+  restaurant: {
+    id: number;
+    name: string;
+    category: string;
+    link: string;
+    roadAddress: string;
+  };
+  followed: boolean;
+  liked: boolean;
+};
+
+export type APISingleFeedResponse = {
+  status: number;
+  response: {
+    content: Content;
+  };
+  error: any;
+};
+
+export type APIFeedResponse = {
+  data: React.SetStateAction<Content[]>;
+  status: number;
+  response: {
+    content: Content[];
+  };
+  error: any;
+};
+
+export type FeedShared = {
+  nickName: string;
+  profileImageUrl: string | null;
+  feedId: number;
+  createdAt: string;
+  updatedAt: string;
+  feedImages: {
+    imageUrl: string;
+  }[];
+  restaurant: {
+    id: number;
+    name: string;
+    category: string;
+    link: string;
+    roadAddress: string;
+  };
+  content: string;
+  likeCount: number;
+  replyCount: number;
+};
