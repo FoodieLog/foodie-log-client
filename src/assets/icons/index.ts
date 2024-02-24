08d7a5a2209eed38c4147dca5e14ac049b345974
ChoEun-Sang <wh94es@gmail.com>
2024-02-24 22:05:47 +0900

    chore: icons, images 파일 정리 및 모듈 불러오기

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,62 @@
+//============ 아이콘 - Common ============
+import CaretDown from "@assets/icons/common/CaretDown.png";
+import ChatCircleText from "@assets/icons/common/ChatCircleText.png";
+import CheckCircle from "@assets/icons/common/CheckCircle.png";
+import Heart from "@assets/icons/common/Heart.png";
+import HeartStraight_2 from "@assets/icons/common/HeartStraight-2.png";
+import HeartStraight from "@assets/icons/common/HeartStraight.png";
+import Phone_2 from "@assets/icons/common/Phone-2.png";
+import Phone from "@assets/icons/common/Phone.png";
+import ShareFat from "@assets/icons/common/ShareFat.png";
+import WarningCircle from "@assets/icons/common/WarningCircle.png";
+import ArrowBack_IOS from "@assets/icons/common/arrow_back_ios.png";
+import AutoreNew_2 from "@assets/icons/common/autorenew-2.png";
+import AutoreNew from "@assets/icons/common/autorenew.png";
+import Favorite from "@assets/icons/common/favorite.png";
+import ImagesMode from "@assets/icons/common/imagesmode.png";
+import LocationOn from "@assets/icons/common/location_on.png";
+import LocationOn_2 from "@assets/icons/common/location_on-2.png";
+import LockReset from "@assets/icons/common/lock_reset.png";
+import MilitaryTech from "@assets/icons/common/military_tech.png";
+import Notifications from "@assets/icons/common/notifications-1.png";
+import Notifications_2 from "@assets/icons/common/notifications-2.png";
+import HeartWithCircle from "@assets/icons/common/HeartWithCircle.png";
+import OpenInNew from "@assets/icons/common/open_in_new.png";
+import PhotoCamera from "@assets/icons/common/photo_camera.png";
+import Restaurant from "@assets/icons/common/restaurant.png";
+import Search from "@assets/icons/common/search.png";
+import Dissatisfied from "@assets/icons/common/dissatisfied.png";
+import Settings from "@assets/icons/common/settings.png";
+
+//============ 아이콘 - Tag ============
+
+export {
+  CaretDown,
+  ChatCircleText,
+  CheckCircle,
+  Heart,
+  HeartStraight_2,
+  HeartStraight,
+  Phone_2,
+  Phone,
+  ShareFat,
+  WarningCircle,
+  ArrowBack_IOS,
+  AutoreNew_2,
+  AutoreNew,
+  Favorite,
+  ImagesMode,
+  LocationOn,
+  LocationOn_2,
+  LockReset,
+  MilitaryTech,
+  Notifications,
+  Notifications_2,
+  HeartWithCircle,
+  OpenInNew,
+  PhotoCamera,
+  Restaurant,
+  Search,
+  Dissatisfied,
+  Settings,
+};
---
+++ src/assets/images/index.ts
@@ -0,0 +1,12 @@
+//============ 이미지 - Restaurants ============
+
+import asian from "@assets/images/restaurants/asian.png";
+import bar from "@assets/images/restaurants/bar.png";
+import cafe from "@assets/images/restaurants/cafe.png";
+import dessert from "@assets/images/restaurants/dessert.png";
+import japanese from "@assets/images/restaurants/japanese.png";
+import korean from "@assets/images/restaurants/korean.png";
+import snack from "@assets/images/restaurants/snack.png";
+import western from "@assets/images/restaurants/western.png";
+
+export { asian, bar, cafe, dessert, japanese, korean, snack, western };
