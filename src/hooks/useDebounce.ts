e63c4fb7c7ccb087225a2098b94a9e292d5da5d2
ChoEun-Sang <wh94es@gmail.com>
2024-02-29 17:19:56 +0900

    feat: useDebounce 커스텀 훅 구현

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+"use client";
+import { useState, useEffect } from "react";
+
+const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [value]);
+
+  return debouncedValue;
+};
+
+export default useDebounce;
