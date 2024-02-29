4d3c11e1f0b69b7b80368743f4c63e500f3e9e28
somin <smhs12@naver.com>
2024-03-01 00:15:33 +0900

    design: 식당 검색 관련 컴포넌트 디자인 수정

--- src/components/PostForm/PostSearch.tsx
+++ src/components/PostForm/PostSearch.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { getSearchShop } from "@services/post";
-import { AiOutlineSearch } from "react-icons/ai";
 import useSignUpStore from "@store/useSignUpStore";
 import PostImage from "@components/PostForm/PostImage";
 import PostContent from "@components/PostForm/PostContent";
@@ -9,34 +8,33 @@ import Header from "@components/Common/Header";
 import { ShopItem } from "@@types/post";
 import PostShopList from "@components/PostForm/PostShopList";
 import SearchInput from "@components/SearchUser/SearchInput";
-import useDebounce from "@/src/hooks/useDebounce";
+import EmptyList from "@components/PostForm/EmptyList";
 
 function PostSearch() {
-  const [query, setQuery] = useState("");
+  const [keyword, setKeyword] = useState("");
   const [shopList, setShopList] = useState<ShopItem[]>([]);
+  const [isShowEmpty, setIsShowEmpty] = useState(false);
   const nextComponent = useSignUpStore((state) => state.nextComponent);
 
-  const debouncedQuery = useDebounce(query, 500);
-
-  useEffect(() => {
-    if (debouncedQuery === "") {
-      setShopList([]);
-    } else {
-      getSearches();
-    }
-  }, [debouncedQuery]);
-
-  const getSearches = async () => {
-    try {
-      const { data } = await getSearchShop(debouncedQuery);
-      setShopList(data.response.documents);
-    } catch (error) {
-      // Todo: 에러 핸들링 추가
-    }
+  const inputKeywordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+    if (!e.target.value) setShopList([]);
+    setIsShowEmpty(false);
   };
 
-  const onChangeInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await getSearchShop(keyword);
+      if (res.data.response.documents.length === 0) {
+        setIsShowEmpty(true);
+        setShopList([]);
+      } else setShopList(res.data.response.documents);
+    } catch (err) {
+      // todo: 사용자 에러 표시
+      console.log("shop search error", err);
+    }
   };
 
   if (nextComponent === "PostImage") {
@@ -49,8 +47,13 @@ function PostSearch() {
     <section className="w-full sm:max-w-[640px]  mx-auto">
       <Header title="식당 검색" back="prePage" />
       <div className="mt-3 mx-3">
-        <SearchInput query={query} setQuery={setQuery} onChangeInputHandler={onChangeInputHandler} />
-        <PostShopList shopList={shopList} />
+        <SearchInput
+          query={keyword}
+          setQuery={setKeyword}
+          onChangeInputHandler={inputKeywordHandler}
+          onSubmit={onSubmit}
+        />
+        {isShowEmpty ? <EmptyList keyword={keyword} /> : <PostShopList shopList={shopList} keyword={keyword} />}
       </div>
     </section>
   );
