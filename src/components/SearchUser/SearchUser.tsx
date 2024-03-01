1ee6941256c1bac63d86f97713520711b1f6f5b5
ChoEun-Sang <wh94es@gmail.com>
2024-03-01 23:45:23 +0900

    refactor: 유저 검색 리스트 컴포넌트 분리

--- src/components/SearchUser/SearchUser.tsx
+++ src/components/SearchUser/SearchUser.tsx
@@ -1,18 +1,21 @@
 "use client";
-import Link from "next/link";
-import UserThumbImg from "@components/Common/Profile/UserThumbImg";
 import SearchInput from "@components/SearchUser/SearchInput";
+import SearchUserList from "@components/SearchUser/SearchUserList";
+import SearchHistory from "@components/SearchUser/SearchHistory";
 import useDebounce from "@/src/hooks/useDebounce";
+import useSearchStore from "@store/useSearchStore";
+import getKey from "@utils/getKey";
 import { useState, useEffect } from "react";
 import { searchUser } from "@services/apiFeed";
-import { UserImage } from "@/public/images";
 import { APIUserSearchResponse } from "@/src/services/apiFeed";
 
 const SearchUser: React.FC = () => {
   const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState<APIUserSearchResponse["response"]["content"] | []>([]);
+  const { searchHistory, setSearchHistory, deleteSearchHistory } = useSearchStore();
 
   const debouncedQuery = useDebounce(query, 500);
+  const key = getKey();
 
   useEffect(() => {
     if (debouncedQuery === "") {
@@ -26,6 +29,7 @@ const SearchUser: React.FC = () => {
     try {
       const { response } = await searchUser(debouncedQuery);
       setSearchResults(response.content);
+      setSearchHistory({ id: key, keyword: debouncedQuery });
     } catch (error) {
       //Todo: 에러 핸들링 추가
     }
@@ -38,27 +42,19 @@ const SearchUser: React.FC = () => {
   return (
     <div className="w-full flex flex-col items-center justify-between px-4 mt-3">
       <SearchInput query={query} setQuery={setQuery} onChangeInputHandler={onChangeInputHandler} />
-      <div className="w-full px-5 mt-4 ">
-        {searchResults.map((user) => (
-          <div key={user.id} className="w-full flex items-center mt-2 hover:bg-slate-100">
-            <Link href={`/main/${user.id}`} className="flex w-12 h-12 flex-shrink-0">
-              {user.profileImageUrl ? (
-                <UserThumbImg src={user.profileImageUrl} />
-              ) : (
-                <UserThumbImg src={UserImage} style="p-1" />
-              )}
-            </Link>
-            <div className="relative flex flex-col pl-2 flex-grow">
-              <Link href={`/main/${user.id}`}>
-                <span className="font-bold overflow-ellipsis overflow-hidden">{user.nickName}</span>
-              </Link>
-              <Link href={`/main/${user.id}`}>
-                <span className="text-sm overflow-ellipsis overflow-hidden">{user.aboutMe}</span>
-              </Link>
-            </div>
-          </div>
-        ))}
-      </div>
+      {searchResults.length ? (
+        <ul className="w-full mt-[16px]">
+          {searchResults.map((user) => (
+            <SearchUserList key={user.id} user={user} />
+          ))}
+        </ul>
+      ) : (
+        <ul className="w-full mt-[16px]">
+          {searchHistory.map((searchBox) => (
+            <SearchHistory key={searchBox.id} searchBox={searchBox} setSearchResults={setSearchResults} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
