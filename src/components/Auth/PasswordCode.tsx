87a2b34a0f2486701af1b9c54c51ca1f80147163
ChaDongmin <cdm1263@gmail.com>
2024-03-02 15:57:22 +0900

    refactor: 코드 입력 UI를 재사용 가능한 CodeInput으로 분리 및 적용

--- src/components/Auth/PasswordCode.tsx
+++ src/components/Auth/PasswordCode.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import Button from "@/src/components/Common/Button";
 import Link from "next/link";
 import { getPasswordCode, getVerificationEmail } from "@/src/services/auth";
@@ -8,34 +8,27 @@ import ChangePassword from "./ChangePassword";
 import AuthHeader from "../Common/Header/Auth";
 import { useToast } from "@/components/ui/use-toast";
 import { TOAST_MESSAGES } from "@/src/constants/toast";
+import CodeInput from "./CodeInput";
 
 function FindPassword() {
   const [isLoading, setIsLoading] = useState(false);
   const [showCodeInput, setShowCodeInput] = useState(false);
-  const [focusedIndex, setFocusedIndex] = useState(0);
   const { toast } = useToast();
   const { nextComponent, setNextComponent } = useSignUpStore();
   const { EMAIL_CODE_SEND_FAILURE, EMAIL_AUTH_FAILURE } = TOAST_MESSAGES;
+  const [email, setEmail] = useState("");
   const [codeData, setCodeData] = useState({
-    email: "",
     firstCode: "",
     secondCode: "",
     thirdCode: "",
     fourthCode: "",
   });
-  const inputRefs: React.RefObject<HTMLInputElement>[] = [
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-  ];
 
   const sendPasswordCodeHandler = async (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const res = await getPasswordCode(codeData.email);
+      await getPasswordCode(email);
       setShowCodeInput(true);
       showCodeInput && setCodeData({ ...codeData, firstCode: "", secondCode: "", thirdCode: "", fourthCode: "" });
     } catch (err) {
@@ -45,21 +38,11 @@ function FindPassword() {
     }
   };
 
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setCodeData({ ...codeData, [name]: value });
-
-    if (value.length >= e.target.maxLength && focusedIndex < inputRefs.length - 1) {
-      inputRefs[focusedIndex + 1].current?.focus();
-      setFocusedIndex(focusedIndex + 1);
-    }
-  };
-
   const onVerificationClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     try {
       const code = codeData.firstCode + codeData.secondCode + codeData.thirdCode + codeData.fourthCode;
-      const res = await getVerificationEmail(codeData.email, code);
+      await getVerificationEmail(email, code);
       setNextComponent("ChangePassword");
     } catch (err) {
       toast(EMAIL_AUTH_FAILURE);
@@ -69,8 +52,18 @@ function FindPassword() {
   };
 
   if (nextComponent === "ChangePassword") {
-    return <ChangePassword email={codeData.email} />;
+    return <ChangePassword email={email} />;
   }
+
+  const findPasswordData = {
+    codeData,
+    setCodeData,
+    isLoading,
+    onClickRetry: sendPasswordCodeHandler,
+    onClickNext: onVerificationClick,
+    retryButtonText: "코드 재전송",
+    nextButtonText: "이메일 인증",
+  };
 
   return (
     <section className="w-full flex flex-col items-center justify-center p-10 h-4/5 sm:w-[600px] sm:border border-gray-300">
@@ -80,8 +73,10 @@ function FindPassword() {
       <form onSubmit={sendPasswordCodeHandler} className="w-full flex flex-col gap-4 mt-10">
         <input
           name="email"
-          value={codeData.email}
-          onChange={onChangeHandler}
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
           className="inputStyles"
           placeholder="이메일 확인"
         />
@@ -96,52 +91,9 @@ function FindPassword() {
       {showCodeInput && (
         <>
           <div className="flex justify-center">
-            <p>{codeData.email}로 전송된 인증코드를 입력해주세요!</p>
+            <p>{email}로 전송된 인증코드를 입력해주세요!</p>
           </div>
-          <div className="flex justify-center gap-2 mt-10">
-            <input
-              name="firstCode"
-              value={codeData.firstCode}
-              onChange={onChangeHandler}
-              type="text"
-              className="code"
-              maxLength={1}
-            />
-            <input
-              name="secondCode"
-              value={codeData.secondCode}
-              onChange={onChangeHandler}
-              type="text"
-              className="code"
-              maxLength={1}
-            />
-            <input
-              name="thirdCode"
-              value={codeData.thirdCode}
-              onChange={onChangeHandler}
-              type="text"
-              className="code"
-              maxLength={1}
-            />
-            <input
-              name="fourthCode"
-              value={codeData.fourthCode}
-              onChange={onChangeHandler}
-              type="text"
-              className="code"
-              maxLength={1}
-            />
-          </div>
-          <div className="w-full">
-            <div className="flex justify-center mb-2">
-              <Button type="button" variant={"text"} onClick={sendPasswordCodeHandler}>
-                코드 재전송
-              </Button>
-            </div>
-            <Button type="button" variant={"primary"} onClick={onVerificationClick}>
-              이메일 인증
-            </Button>
-          </div>
+          <CodeInput {...findPasswordData} />
         </>
       )}
     </section>
--- src/components/Auth/SignUpCode.tsx
+++ src/components/Auth/SignUpCode.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { useState, useRef } from "react";
-import Button from "@/src/components/Common/Button";
+import { useState } from "react";
 import useSignUpStore from "@/src/store/useSignUpStore";
 import { sendEmailCode, getVerificationEmail } from "@/src/services/auth";
 import AuthHeader from "../Common/Header/Auth";
 import { useToast } from "@/components/ui/use-toast";
 import { TOAST_MESSAGES } from "@/src/constants/toast";
+import CodeInput from "./CodeInput";
+import { CodeData } from "@/src/types/signupTypes";
 
 function SignUpCode() {
   const {
@@ -13,24 +14,16 @@ function SignUpCode() {
     user: { email },
   } = useSignUpStore();
   const [isLoading, setIsLoading] = useState(false);
-  const [codeData, setCodeData] = useState({
+  const [codeData, setCodeData] = useState<CodeData>({
     firstCode: "",
     secondCode: "",
     thirdCode: "",
     fourthCode: "",
   });
-  const [focusedIndex, setFocusedIndex] = useState(0);
-  const inputRefs: React.RefObject<HTMLInputElement>[] = [
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-  ];
   const { toast } = useToast();
   const { EMAIL_CODE_SEND_FAILURE, EMAIL_CODE_SEND_SUCCESS, EMAIL_CODE_AUTH_FAILURE } = TOAST_MESSAGES;
 
-  const ResendClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+  const resendClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     setIsLoading(true);
     try {
       await sendEmailCode(email);
@@ -42,7 +35,7 @@ function SignUpCode() {
     }
   };
 
-  const NextClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+  const nextClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setIsLoading(true);
     const code = codeData.firstCode + codeData.secondCode + codeData.thirdCode + codeData.fourthCode;
@@ -56,15 +49,16 @@ function SignUpCode() {
     }
   };
 
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setCodeData({ ...codeData, [name]: value });
-
-    if (value.length >= e.target.maxLength && focusedIndex < inputRefs.length - 1) {
-      inputRefs[focusedIndex + 1].current?.focus();
-      setFocusedIndex(focusedIndex + 1);
-    }
+  const signUpData = {
+    codeData,
+    setCodeData,
+    isLoading,
+    onClickRetry: resendClick,
+    onClickNext: nextClick,
+    retryButtonText: "코드 재전송",
+    nextButtonText: "다음",
   };
+
   return (
     <section className="flex flex-col items-center justify-center p-10 h-4/5 sm:w-[600px] sm:border border-gray-300">
       <AuthHeader back="preComponent" />
@@ -73,55 +67,8 @@ function SignUpCode() {
         <p>
           <span>{email || "이메일"}</span>로 전송된 인증코드를 입력해주세요!
         </p>
-        <div className="flex justify-center gap-2 mt-10">
-          <input
-            name="firstCode"
-            value={codeData.firstCode}
-            onChange={onChangeHandler}
-            type="text"
-            className="code"
-            maxLength={1}
-            ref={inputRefs[0]}
-          />
-          <input
-            name="secondCode"
-            value={codeData.secondCode}
-            onChange={onChangeHandler}
-            type="text"
-            className="code"
-            maxLength={1}
-            ref={inputRefs[1]}
-          />
-          <input
-            name="thirdCode"
-            value={codeData.thirdCode}
-            onChange={onChangeHandler}
-            type="text"
-            className="code"
-            maxLength={1}
-            ref={inputRefs[2]}
-          />
-          <input
-            name="fourthCode"
-            value={codeData.fourthCode}
-            onChange={onChangeHandler}
-            type="text"
-            className="code"
-            maxLength={1}
-            ref={inputRefs[3]}
-          />
-        </div>
       </div>
-      <div className="w-full">
-        <div className="flex justify-center mb-2">
-          <Button type="button" variant={"text"} onClick={ResendClick} disabled={isLoading}>
-            코드 재전송
-          </Button>
-        </div>
-        <Button type="button" variant={"primary"} onClick={NextClick}>
-          다음
-        </Button>
-      </div>
+      <CodeInput {...signUpData} />
     </section>
   );
 }
