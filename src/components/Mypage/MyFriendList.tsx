90523fd1bdd213cf6970b54d7b60eb8c0519f66f
ChoEun-Sang <wh94es@gmail.com>
2024-03-09 13:45:59 +0900

    refactor: 팔로우 목록 컴포넌트 - Drawer 적용 및 팔로우 버튼 컴포넌트 수정

--- src/components/Mypage/MyFriendList.tsx
+++ src/components/Mypage/MyFriendList.tsx
@@ -1,86 +1,56 @@
 "use client";
-
 import Image from "next/image";
 import Link from "next/link";
-import { followUser, unfollowUser } from "@services/apiFeed";
-import { MyFriendListProps } from "@@types/mypage";
+import Drawer from "@components/Common/Drawer/Drawer";
+import FollowButton from "@components/Common/Button/FollowButton";
+import { ListData } from "@@types/mypage";
 
-function MyFriendList({ data, friendListTitle, reload, setReload, updateFollow, updateFollower }: MyFriendListProps) {
-  const onClickFollowing = async (e: React.MouseEvent<HTMLButtonElement>, Id: number) => {
-    e.preventDefault();
-    e.stopPropagation();
-    try {
-      const res = await unfollowUser(Id);
-      setReload(!reload);
-      if (friendListTitle === "팔로우") {
-        updateFollow();
-      } else {
-        updateFollower();
-      }
-    } catch (err) {
-      console.log("언팔 에러", err);
-    }
-  };
+export interface MyFriendListProps {
+  data: ListData[];
+  object: string;
+  setShowFriendList: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-  const onClickFollow = async (e: React.MouseEvent<HTMLButtonElement>, Id: number) => {
-    e.preventDefault();
-    e.stopPropagation();
-    try {
-      const res = await followUser(Id);
-      setReload(!reload);
-      if (friendListTitle === "팔로우") {
-        updateFollow();
-      } else {
-        updateFollower();
-      }
-    } catch (err) {
-      console.log("팔 에러", err);
+function MyFriendList({ data, object, setShowFriendList }: MyFriendListProps) {
+  const clickBgToClose = (e: React.MouseEvent<HTMLDivElement> | React.KeyboardEvent<HTMLDivElement>) => {
+    if (setShowFriendList && e.target === e.currentTarget) {
+      setShowFriendList((prev: boolean) => !prev);
     }
   };
 
   return (
-    <section className="flex flex-col items-center p-5 gap-5 bg-white">
-      <h3>{friendListTitle}</h3>
-      {data.length > 0 ? (
-        <ul className="w-full">
-          {data.map((item) => (
-            <li key={item.userId} className=" w-full mb-3 flex justify-between ">
-              <Link href={`/main/${item.userId}`} className="flex items-center gap-3 overflow-hidden">
-                <div className="relative w-[30px] h-[30px] flex flex-col  justify-center items-center text-center rounded-lg overflow-hidden">
-                  <Image
-                    width={30}
-                    height={30}
-                    src={item.profileImageUrl || "/images/userImage.png"}
-                    alt={item.nickName}
-                  />
-                </div>
-                <span>{item.nickName}</span>
-              </Link>
-
-              {item.followed ? (
-                <button
-                  type="button"
-                  onClick={(e) => onClickFollowing(e, item.userId)}
-                  className="px-2 border border-gray-300 text-sm rounded-md cursor-pointer"
-                >
-                  팔로잉
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  onClick={(e) => onClickFollow(e, item.userId)}
-                  className="px-2 bg-mint text-white text-sm rounded-md cursor-pointer"
-                >
-                  팔로우
-                </button>
-              )}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>데이터가 없습니다.</p>
-      )}
-    </section>
+    <div
+      onClick={clickBgToClose}
+      className="bg-gray-300 bg-opacity-50 fixed w-screen h-screen flex justify-center items-center top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 max-h-full"
+    >
+      <Drawer>
+        <section className="flex flex-col items-center p-5 gap-5 bg-white">
+          <h3>{object}</h3>
+          {data?.length > 0 ? (
+            <ul className="w-full">
+              {data.map((user) => (
+                <li key={user.userId} className=" w-full mb-3 flex justify-between ">
+                  <Link href={`/main/${user.userId}`} className="flex items-center gap-3 overflow-hidden">
+                    <div className="relative w-[30px] h-[30px] flex flex-col  justify-center items-center text-center rounded-lg overflow-hidden">
+                      <Image
+                        width={30}
+                        height={30}
+                        src={user.profileImageUrl || "/images/userImage.png"}
+                        alt={user.nickName}
+                      />
+                    </div>
+                    <span>{user.nickName}</span>
+                  </Link>
+                  <FollowButton isFollowed={user.followed} userId={user.userId} className={"w-[64px] text-[14px]"} />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>데이터가 없습니다.</p>
+          )}
+        </section>
+      </Drawer>
+    </div>
   );
 }
 
