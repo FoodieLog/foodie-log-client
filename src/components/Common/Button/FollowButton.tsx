f3de6b2fc3f7f6164ec881e39b70511cc4bbf107
ChoEun-Sang <wh94es@gmail.com>
2024-03-09 13:34:37 +0900

    refactor: 팔로우 버튼 useFollowMutation 훅 적용

--- src/components/Common/Button/FollowButton.tsx
+++ src/components/Common/Button/FollowButton.tsx
@@ -1,62 +1,36 @@
-import { getFeedList, getFeedListByUserId, getSingleFeed, followUser, unfollowUser } from "@services/apiFeed";
 import { UserPlus } from "@assets/icons";
+import useFollowMutations from "@hooks/mutations/useFollowMutaton";
 
 interface FollowButtonProps {
   isFollowed: boolean;
   userId: number;
-  size: string;
+  className: string;
+  icon?: boolean;
 }
 
-function FollowButton({ isFollowed, userId, size }: FollowButtonProps) {
+function FollowButton({ isFollowed, userId, className, icon = false }: FollowButtonProps) {
+  const { followMutation, unfollowMutation } = useFollowMutations(userId);
+
   const clickFollowBtnHandler = async () => {
-    try {
-      if (!isFollowed) {
-        const response = await followUser(userId);
-        console.log("팔로우 버튼", response);
-      } else {
-        const response = await unfollowUser(userId);
-        console.log("언팔 버튼", response);
-      }
-    } catch (error) {
-      console.log("팔로우 버튼 에러", error);
+    if (isFollowed) {
+      unfollowMutation.mutate();
+    } else {
+      followMutation.mutate();
     }
   };
-  // 임시 팔로우 상태를 반영하는 state를 하나 만들던지, 다른 방식을 찾던지
-  //   const updateFollowStatus = async (userId: number, newStatus: boolean) => {
-  //   let response;
-  //   try {
-  //     if (newStatus) {
-  //       response = await followUser(userId);
-  //     } else {
-  //       response = await unfollowUser(userId);
-  //     }
-
-  //     if ((newStatus && response.status === 201) || (!newStatus && response.status === 204)) {
-  //       setFeedsData((prevData) => {
-  //         return prevData.map((content) => {
-  //           if (content.feed.userId === userId) {
-  //             return { ...content, followed: newStatus };
-  //           }
-  //           return content;
-  //         });
-  //       });
-  //     }
-  //   } catch (error) {
-  //     console.error("Failed to update follow state:", error);
-  //   }
-  // };
 
   return (
     <button
       type="button"
       onClick={clickFollowBtnHandler}
-      className={isFollowed ? `followingBtn ${size}` : `followBtn ${size}`}
+      className={isFollowed ? `followingBtn ${className}` : `followBtn ${className}`}
     >
       {isFollowed ? (
         <p>팔로잉</p>
       ) : (
         <p className="flex gap-1 justify-center items-center">
-          <UserPlus /> 팔로우
+          {icon && <UserPlus />}
+          <p>팔로우</p>
         </p>
       )}
     </button>
