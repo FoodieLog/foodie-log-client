cb14c161848c19b014cee024f248577f633c4e6c
ChoEun-Sang <wh94es@gmail.com>
2024-03-14 00:26:31 +0900

    refactor: TextArea 스타일 수정

--- src/components/Common/TextArea/index.tsx
+++ src/components/Common/TextArea/index.tsx
@@ -11,11 +11,11 @@ function TextArea({ maxLength, title, className, ...props }: TextAreaProps) {
   const { id, value } = props;
   return (
     <div className="relative">
-      <label htmlFor={id} className="absolute top-[10px] left-[10px] text-sm text-gray-4">
+      <label htmlFor={id} className="start-[16px] absolute top-[10px] left-[10px] text-[12px] text-gray-4">
         {title}
       </label>
       <textarea {...props} className={cn("w-full h-[162px] resize-none focus:outline-none", className)} />
-      <p className="absolute bottom-[10px] right-[10px] text-gray-3">
+      <p className="absolute bottom-[14px] right-[10px] text-[14px] text-gray-3">
         {value?.toString().length}/{maxLength}
       </p>
     </div>
