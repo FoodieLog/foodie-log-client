dee4f1df3a50ab3ae2fe1c762952fc10cf6d0484
ChoEun-Sang <wh94es@gmail.com>
2024-03-18 20:33:24 +0900

    refactor: UserThumbnail Size Props 추가

--- src/components/Common/Thumbnail/UserThumbnail.tsx
+++ src/components/Common/Thumbnail/UserThumbnail.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 interface UserThumbnailProps {
   profileImgUrl: string | null;
   userId: string | number;
+  size: string;
 }
 
-function UserThumbnail({ profileImgUrl, userId }: UserThumbnailProps) {
+function UserThumbnail({ profileImgUrl, userId, size }: UserThumbnailProps) {
   return (
-    <Link href={`/main/${userId}`} className="relative w-12 h-12 rounded-full overflow-hidden shrink-0">
+    <Link href={`/main/${userId}`} className={`${size} relative rounded-full overflow-hidden shrink-0`}>
       <Image src={profileImgUrl || "/images/userImage.png"} alt="사용자 썸네일" layout="fill" objectFit="cover" />
     </Link>
   );
