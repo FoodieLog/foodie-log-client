676fb3bf6093d5f107e99baaa62dcda9983336e6
ChoEun-Sang <wh94es@gmail.com>
2024-03-19 16:01:31 +0900

    refactor: use-toast.ts import 수정

--- components/ui/use-toast.ts
+++ components/ui/use-toast.ts
@@ -1,6 +1,5 @@
-// Inspired by react-hot-toast library
-import * as React from "react";
-
+"use client";
+import { useState, useEffect } from "react";
 import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
 
 const TOAST_LIMIT = 1;
@@ -166,9 +165,9 @@ function toast({ ...props }: Toast) {
 }
 
 function useToast() {
-  const [state, setState] = React.useState<State>(memoryState);
+  const [state, setState] = useState<State>(memoryState);
 
-  React.useEffect(() => {
+  useEffect(() => {
     listeners.push(setState);
     return () => {
       const index = listeners.indexOf(setState);
