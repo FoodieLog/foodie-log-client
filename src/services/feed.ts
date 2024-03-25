95ffb611cab63e6a6bdc112ec0038e484af4f6ee
ChaDongmin <cdm1263@gmail.com>
2024-03-26 00:51:17 +0900

    refactor: api 변경에 맞게 메인 피드 api 초건문 변경

--- src/services/feed.ts
+++ src/services/feed.ts
@@ -33,15 +33,11 @@ export const getFeedListByUserId = async (userId: number, feedId: number) => {
 
 /** 피드 목록 요청 */
 export const getFeedList = async (feedId: number, category?: RestaurantCategory) => {
-  let res;
-  if (feedId === 0) {
-    res = await userRequest.get(`api/feed/list`);
-  } else {
-    res = await userRequest.get(`api/feed/list?feedId=${feedId}`);
-    // 추후 카테고리 기능 완성되면 아래 로직으로 사용
-    // res = await userRequest.get(`api/feed/list?category=${category}&last=${feedId}`);
-  }
-  return res;
+  if (feedId !== 0) return await userRequest.get(`api/feed/list?category=${category}&last=${feedId}`);
+
+  return category
+    ? await userRequest.get(`api/feed/list?category=${category}`)
+    : await userRequest.get(`api/feed/list`);
 };
 
 /** 싱글 피드 요청 */
