351eac708056e1518f420b99fcd5ba12809edd88
ChoEun-Sang <wh94es@gmail.com>
2024-04-03 01:18:31 +0900

    feat: AuthCheck 카카오 토큰 재발급 로직 추가

--- src/components/Auth/AuthCheck.ts
+++ src/components/Auth/AuthCheck.ts
@@ -6,15 +6,15 @@ import { reissueTokens } from "@services/auth";
 import { useToast } from "@/components/ui/use-toast";
 import useLogout from "@hooks/useLogout";
 import { minutesInMilliseconds } from "@utils/date";
+import { tokenLoader } from "@utils/token";
+import { getKaKaoRefreshToken, logoutKaKaoToken } from "@services/kakao";
 
 const AuthCheck: React.FC = () => {
   const { toast } = useToast();
-  const { user, setUser, setTokenExpiry } = useUserStore();
+  const { user, setUser, setTokenExpiry, clearUser } = useUserStore();
   const router = useRouter();
   const pathname = usePathname();
   const { logout } = useLogout();
-
-  // const kakaoRefreshToken = tokenLoader();
 
   const isTokenExpired = user.tokenExpiry ? Date.now() > user.tokenExpiry : true;
 
@@ -22,6 +22,7 @@ const AuthCheck: React.FC = () => {
   useEffect(() => {
     const validateTokens = async () => {
       const useStorage = localStorage.getItem("user-storage");
+
       if (useStorage) {
         const { accessToken } = JSON.parse(useStorage).state.user;
 
@@ -51,27 +52,27 @@ const AuthCheck: React.FC = () => {
     validateTokens();
   }, [user.accessToken, user.tokenExpiry, router, pathname, isTokenExpired, setUser, setTokenExpiry]);
 
-  //카카오 리프레쉬 토큰 로직
+  // 카카오 리프레쉬 토큰 로직
+  useEffect(() => {
+    const kakaoRefreshToken = tokenLoader();
+    const token = localStorage.getItem("kakaoRefresh");
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("kakaoRefresh");
+    if (token && kakaoRefreshToken === "EXPIRED") {
+      const getKakaoRefresh = async () => {
+        try {
+          const { data } = await getKaKaoRefreshToken(token);
 
-  //   if (kakaoRefreshToken === "EXPIRED" && token) {
-  //     const getKakaoRefresh = async () => {
-  //       try {
-  //         const { data } = await getKaKaoRefreshToken(token);
-
-  //         setUser({ accessToken: data.access_token });
-  //         localStorage.setItem("kakaoRefresh", data.refresh_token);
-  //       } catch (err) {
-  //         toast({ description: "토큰이 유효하지 않습니다.\n다시 로그인해 주세요!" });
-  //         Logout();
-  //       }
-  //     };
-  //     getKakaoRefresh();
-  //   }
-
-  // }, []);
+          setUser({ accessToken: data.access_token });
+          localStorage.setItem("kakaoRefresh", data.refresh_token);
+        } catch (err) {
+          toast({ description: "토큰이 유효하지 않습니다.\n다시 로그인해 주세요!" });
+          await logoutKaKaoToken();
+          clearUser();
+        }
+      };
+      getKakaoRefresh();
+    }
+  }, [router, pathname, setUser]);
 
   return null;
 };
