a560adfd01f5afcbde0af02bcd9a8497e474fa31
ChaDongmin <cdm1263@gmail.com>
2024-04-04 07:24:33 +0900

    refactor: 마이페이지 지도 높이 조정

--- src/components/Mypage/MyMap.tsx
+++ src/components/Mypage/MyMap.tsx
@@ -5,11 +5,12 @@ import useMyMapQuery from "@hooks/queries/useMyMapQuery";
 
 function MyMap({ userId }: MyMapProps) {
   const { data } = useMyMapQuery(userId);
+  const mapHeight = window.innerHeight - 370;
 
   return (
     <section className="w-full sm:max-w-[640px] mx-auto">
       <div className="flex flex-col items-center">
-        <MyListMap mapData={data?.myMap} />
+        <MyListMap mapData={data?.myMap} size={{ width: "100%", height: `${mapHeight}px` }} />
       </div>
     </section>
   );
