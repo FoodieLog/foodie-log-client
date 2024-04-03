be4fcf1462ed1351a8b3d3622cb0ff4a269d5567
ChoEun-Sang <wh94es@gmail.com>
2024-04-03 15:33:20 +0900

    refactor: 마이 페이지 헤더 수정

--- src/components/Mypage/MyProfile.tsx
+++ src/components/Mypage/MyProfile.tsx
@@ -10,11 +10,13 @@ import useMyPageQuery from "@/src/hooks/queries/useMyPageQuery";
 import useMyFollowersQuery from "@hooks/queries/useMyFollowersQuery";
 import useFollowMutations from "@hooks/mutations/useFollowMutaton";
 import UserThumbnail from "@components/Common/Thumbnail/UserThumbnail";
+import { useUserStore } from "@/src/store/useUserStore";
 
 function Mypage({ userId, option }: Mypage) {
   const [showFriendList, setShowFriendList] = useState(false);
   const [isClient, setIsClient] = useState(false);
   const [object, setObject] = useState("");
+  const { user } = useUserStore();
 
   const router = useRouter();
 
@@ -56,7 +58,11 @@ function Mypage({ userId, option }: Mypage) {
   return (
     <section className="w-full sm:max-w-[640px] mx-auto h-[100vh] overflow-hidden relative">
       <MyFriendList data={myFollowerData} object={object} isOpener={showFriendList} setIsOpener={setShowFriendList} />
-      <Header title={option === "타인" ? "" : "마이"} back="prePage" option={option} />
+      <Header
+        title={user.id === userId ? "마이" : ""}
+        back="prePage"
+        option={user.id === userId ? "설정 및 개인정보" : option}
+      />
       <main className="px-4 py-5 space-y-3">
         <div className="flex justify-between items-center mb-3 gap-6 shrink-0">
           <UserThumbnail profileImgUrl={data?.profileImageUrl} userId={data?.userId} size="w-[90px] h-[90px]" />
@@ -81,16 +87,16 @@ function Mypage({ userId, option }: Mypage) {
         <div className="mb-[32px]">
           <p>{data?.aboutMe}</p>
         </div>
-        {option === "타인" ? (
+        {user.id === userId ? (
           <div className="mb-[32px]">
-            <Button type="button" variant={data.followed ? "secondary" : "primary"} onClick={clickFollowBtn}>
-              {data.followed ? "팔로잉" : "팔로우"}
+            <Button type="button" variant="secondary" onClick={clickProfileEdit}>
+              프로필 수정
             </Button>
           </div>
         ) : (
           <div className="mb-[32px]">
-            <Button type="button" variant="secondary" onClick={clickProfileEdit}>
-              프로필 수정
+            <Button type="button" variant={data.followed ? "secondary" : "primary"} onClick={clickFollowBtn}>
+              {data.followed ? "팔로잉" : "팔로우"}
             </Button>
           </div>
         )}
