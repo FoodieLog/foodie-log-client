872c113ee738945558cadb192a9a42af9b8e346c
ChoEun-Sang <wh94es@gmail.com>
2024-04-03 15:31:03 +0900

    refactor: GNB, LeftSidebar 마이페이지 경로 수정

--- src/components/Common/Navigation/GlobalNavigation.tsx
+++ src/components/Common/Navigation/GlobalNavigation.tsx
@@ -2,11 +2,51 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { globalNavigation } from "@constants";
+// import { globalNavigation } from "@constants";
 import useShowPartial from "@hooks/useShowPartial";
+import {
+  SpaceDashboard,
+  SpaceDashboardCheck,
+  MapIcon,
+  MapIconCheck,
+  ThumbUp,
+  ThumbUpCheck,
+  Person,
+  PersonCheck,
+} from "@assets/icons";
+import { NavItemType } from "@@types/navigation";
+import { useUserStore } from "@/src/store/useUserStore";
 
 function GlobalNavigation() {
   const { pathname, isShow } = useShowPartial();
+  const { user } = useUserStore();
+
+  const globalNavigation: NavItemType[] = [
+    {
+      icon: SpaceDashboard,
+      icon_checked: SpaceDashboardCheck,
+      label: "피드",
+      route: "/main/home",
+    },
+    {
+      icon: MapIcon,
+      icon_checked: MapIconCheck,
+      label: "내 지도",
+      route: "/main/liked",
+    },
+    {
+      icon: ThumbUp,
+      icon_checked: ThumbUpCheck,
+      label: "지역별",
+      route: "/main/recommend",
+    },
+    {
+      icon: Person,
+      icon_checked: PersonCheck,
+      label: "마이",
+      route: `/main/${user.id}`,
+    },
+  ];
 
   return (
     <>
